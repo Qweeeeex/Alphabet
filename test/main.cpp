3b1556c4c4649142dafa98d37e4be7f92b288b73
Qweeeeex <vbvbvbvb72@gmail.com>
2020-05-23 12:34:24 +0700

    Update tests

--- test/main.cpp
+++ test/main.cpp
@@ -5,19 +5,19 @@
 #include "../lib/catch.hpp"
 
 TEST_CASE("TestCheck1", "SortIsOk1") {
-	int test1 = check("b", "b");
+	int test1 = check("apple", "apple");
 	int expected = 0;
 	REQUIRE(test1 == expected);
 }
 
 TEST_CASE("TestCheck2", "SortIsOk2") {
-	int test1 = check("a", "b");
+	int test1 = check("apple", "car");
 	int expected = -1;
 	REQUIRE(test1 == expected);
 }
 
 TEST_CASE("TestCheck3", "SortIsOk3") {
-	int test1 = check("b", "a");
+	int test1 = check("job", "car");
 	int expected = 1;
 	REQUIRE(test1 == expected);
 }
