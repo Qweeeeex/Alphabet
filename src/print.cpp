ed62099b1ab6b654cb4a2db2e6953d3534d7c445
Qweeeeex <vbvbvbvb72@gmail.com>
2020-05-23 13:36:22 +0700

    Update print.cpp

--- src/print.cpp
+++ src/print.cpp
@@ -5,10 +5,10 @@ void print(char (&zn1)[maxzn][maxword], int* k)
     cout << "Words in alphabet order:" << endl;
     for (int i = 0; i != *k; i++) {
         for (int j = 0; zn1[i][j] != '\0'; j++) {
-            if ((zn1[i][j] != '\n') && (zn1[i][j] != '\0') &&
-                (zn1[i][j] != '.') && (zn1[i][j] != ',') &&
-                (zn1[i][j] != ':') && (zn1[i][j] != ';') &&
-                (zn1[i][j] != '!') && (zn1[i][j] != '?'))
+            if ((zn1[i][j] != '\n') && (zn1[i][j] != '\0') && (zn1[i][j] != '.')
+                && (zn1[i][j] != ',') && (zn1[i][j] != ':')
+                && (zn1[i][j] != ';') && (zn1[i][j] != '!')
+                && (zn1[i][j] != '?'))
                 cout << zn1[i][j];
             zn1[i][j] = 0;
         }
